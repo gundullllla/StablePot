b47b6ce6b33a9ef641cca2c37355ce536f2a7658
gundullllla <gundullllla@proton.me>
2025-07-01 22:01:53 +0200

    Update main.cpp

--- examples/EMA1_Basic/src/main.cpp
+++ examples/EMA1_Basic/src/main.cpp
@@ -1,24 +1,20 @@
-// examples/StablePot4_PerPotConfig/src/main.cpp
 #include <StablePot.h>
 
-StablePot knob1(15, StablePot::STABLEPOT4);
-StablePot knob2(32, StablePot::STABLEPOT4);
+// Using EMA1 algorithm with its default preset
+StablePot knob(15, StablePot::EMA1);
 
 void setup() {
   Serial.begin(115200);
-  
-  // Customize individually
-  knob1.configure(0.3f, 0.75f, 0.002f, 0.0007f, 150, 4);
-  knob2.configure(0.2f, 0.8f, 0.001f, 0.0005f, 100, 5);
 }
 
 void loop() {
-  knob1.update();
-  knob2.update();
+  knob.update();
   
-  Serial.print(knob1.getProcessedADC());
+  Serial.print(knob.getRawValue(), 4);
   Serial.print('\t');
-  Serial.println(knob2.getProcessedADC());
+  Serial.print(knob.getSmoothedValue(), 4);
+  Serial.print('\t');
+  Serial.println(knob.getProcessedValue(), 4);
   
   delay(1);
 }
